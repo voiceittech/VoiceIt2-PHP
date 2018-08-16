146aa0e52f364713a848760770fdb64303ee4b4c
gilgameshskytrooper <me@gilgameshskytrooper.io>
2018-08-16 12:04:42 -0500

    this commit wont trigger a release

--- tests/packagist.sh
+++ tests/packagist.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-version=$(curl -s https://api.github.com/repos/voiceittech/VoiceIt2-PHP/releases/latest | grep '"tag_name":' |sed -E 's/.*"([^"]+)".*/\1/' | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
-curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-PHP/releases
-curl -XPOST -H'content-type:application/json' 'https://packagist.org/api/update-package?username='$PACKAGISTUSERNAME'&apiToken='$PACKAGISTAPITOKEN'' -d'{"repository":{"url":"https://github.com/voiceittech/VoiceIt2-PHP"}}'
+commit=$(git log -1 --pretty=%B | head -n 1)
+if [[ $commit = *"Release"* ]];
+then
+  version=$(curl -s https://api.github.com/repos/voiceittech/VoiceIt2-PHP/releases/latest | grep '"tag_name":' |sed -E 's/.*"([^"]+)".*/\1/' | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
+  curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-PHP/releases
+  curl -XPOST -H'content-type:application/json' 'https://packagist.org/api/update-package?username='$PACKAGISTUSERNAME'&apiToken='$PACKAGISTAPITOKEN'' -d'{"repository":{"url":"https://github.com/voiceittech/VoiceIt2-PHP"}}'
+fi
