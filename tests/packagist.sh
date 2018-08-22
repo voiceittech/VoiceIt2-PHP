cbc1878533a533281ccff2dfa2536c4e7b08ac50
gilgameshskytrooper <me@gilgameshskytrooper.io>
2018-08-22 16:19:30 -0500

    update upload.sh to trigger RELEASEMINOR functionality

--- tests/packagist.sh
+++ tests/packagist.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 commit=$(git log -1 --pretty=%B | head -n 1)
-if [[ $commit = *"Release"* ]];
+version=$(echo $(curl -s https://api.github.com/repos/voiceittech/VoiceIt2-PHP/releases/latest | grep '"tag_name":' |sed -E 's/.*"([^"]+)".*/\1/') | tr "." "\n")
+set -- $version
+major=$1
+minor=$2
+patch=$3
+
+if [[ $commit = *"RELEASE"* ]];
 then
-  version=$(curl -s https://api.github.com/repos/voiceittech/VoiceIt2-PHP/releases/latest | grep '"tag_name":' |sed -E 's/.*"([^"]+)".*/\1/' | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
+  if [[ $commit = *"RELEASEMAJOR"* ]];
+  then
+    major=$(($major+1))
+    minor=0
+    patch=0
+  elif [[ $commit = *"RELEASEMINOR"* ]];
+  then
+    minor=$(($minor+1))
+    patch=0
+  elif [[ $commit = *"RELEASEPATCH"* ]];
+  then
+    patch=$(($patch+1))
+  else
+    echo "Must specify RELEASEMAJOR, RELEASEMINOR, or RELEASEPATCH in the title." 1>&2
+    exit 1
+  fi
+
+  version=$major'.'$minor'.'$patch
   curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-PHP/releases
   curl -XPOST -H'content-type:application/json' 'https://packagist.org/api/update-package?username='$PACKAGISTUSERNAME'&apiToken='$PACKAGISTAPITOKEN'' -d'{"repository":{"url":"https://github.com/voiceittech/VoiceIt2-PHP"}}'
+
 fi
