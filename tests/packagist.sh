a07eb3165b23faedad5bafc7159bad740dc8d108
gilgameshskytrooper <me@gilgameshskytrooper.io>
2018-08-22 16:28:18 -0500

    update upload.sh to trigger RELEASEMINOR functionality and add 10 seconds between github release command and packagist update command

--- tests/packagist.sh
+++ tests/packagist.sh
@@ -27,6 +27,6 @@ then
 
   version=$major'.'$minor'.'$patch
   curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-PHP/releases
+  sleep 10
   curl -XPOST -H'content-type:application/json' 'https://packagist.org/api/update-package?username='$PACKAGISTUSERNAME'&apiToken='$PACKAGISTAPITOKEN'' -d'{"repository":{"url":"https://github.com/voiceittech/VoiceIt2-PHP"}}'
-
 fi
